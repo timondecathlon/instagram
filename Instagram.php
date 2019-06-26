208696cc8374ded2cf124f661629dbd299010c3c
timondecathlon <sonicstyle99@gmail.com>
2019-06-26 15:27:31 +0300

    Update Instagram.php

--- Instagram.php
+++ Instagram.php
@@ -1,87 +1,152 @@
-<?
 class Instagram
 {
 		
-	public function __construct($link) 
+	public function __construct($link)
 	{		
 		$this->link = $link;
-		$ch = curl_init($this->link);  
+		//another way to get html through curl library
+		/*
+		$ch = curl_init($this->link);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $data = curl_exec($ch);
+        	$data = curl_exec($ch);
+		*/
+        	$data = file_get_contents($this->link);
 		//get json string
 		$left = explode('<script type="text/javascript">window._sharedData = ', $data);//explode the part we dont use from the left
 		$right = explode(";</script>", $left[1]); //explode the part we dont use from the right
 		$jsonData = $right[0];  //get json string between characters
-        $this->data = json_decode($jsonData); //decoding and get the array
-	}  
-	
-	public function getPostImage($num) 
-	{
-		return $this->data->entry_data->ProfilePage[0]->graphql->user->edge_owner_to_timeline_media->edges[$num]->node->thumbnail_src;
+        	$this->data = json_decode($jsonData); //decoding and get the array
 	}
+
+
+    public function getLink()
+    {
+        return  $this->link;
+    }
+
+    /**
+     * Getting link as Post
+     */
+    public function getPost()
+    {
+        return $this->data->entry_data->PostPage[0]->graphql->shortcode_media;
+    }
+
+    public function getPostText()
+    {
+        return $this->getPost()->edge_media_to_caption->edges[0]->node->text;
+    }
+
+    public function getPostPhoto()
+    {
+        return $this->getPost()->display_resources[0]->src;
+    }
+
+    public function getPostComments()
+    {
+        return $this->getPost()->edge_media_to_comment;
+    }
+
+    public function getPostCommentsCount()
+    {
+        return $this->getPostComments()->count;
+    }
+
+    public function getPostLikesCount()
+    {
+        return $this->getPost()->edge_media_preview_like->count;
+    }
+
+
+    /**
+     * Getting link as Profile
+     */
+
+    public function getProfileInfo()
+    {
+        return  $this->data->entry_data->ProfilePage[0]->graphql->user;
+    }
+
+    public function getProfileName()
+    {
+        return $this->getProfileInfo()->username;
+    }
+
+    public function getProfileFullName()
+    {
+        return $this->getProfileInfo()->full_name;
+    }
+
+    public function getProfileDescription()
+    {
+        return $this->getProfileInfo()->biography;
+    }
+
+    public function getProfileUrl()
+    {
+        return  $this->getProfileInfo()->external_url;
+    }
+
+    public function getProfilePhoto()
+    {
+        return $this->getProfileInfo()->profile_pic_url_hd;
+    }
+
+    public function getProfilePhotoThumb()
+    {
+        return  $this->getProfileInfo()->profile_pic_url;
+    }
+
+    public function getProfileFollowers()
+    {
+        return  $this->getProfileInfo()->edge_followed_by->count;
+    }
+
+    public function getProfileFollows()
+    {
+        return  $this->getProfileInfo()->edge_follow->count;
+    }
+
+
+    /**
+     * Getting Profile's Post by count
+     */
+    public function getAllPosts()
+    {
+        return $this->data->entry_data->ProfilePage[0]->graphql->user->edge_owner_to_timeline_media;
+    }
+
+    public function getProfilePost($num)
+    {
+        return $this->getAllPosts()->edges[$num]->node;
+    }
+
+    public function getProfilePostText($num)
+    {
+        return $this->getProfilePost($num)->edge_media_to_caption->edges[0]->node->text;
+    }
+
+    public function getProfilePostImage($num)
+    {
+        return $this->getProfilePost($num)->thumbnail_src;
+    }
 	
-	public function getPostText($num) 
-	{
-		return $this->data->entry_data->ProfilePage[0]->graphql->user->edge_owner_to_timeline_media->edges[$num]->node->edge_media_to_caption->edges[0]->node->text; 
-	}
+
+
+
 	
-	public function getPost($num) 
-	{
-		return $this->data->entry_data->ProfilePage[0]->graphql->user->edge_owner_to_timeline_media->edges[$num]->node->edge_media_to_caption->edges[0]->node;	
-	}
+
+
+
+
+
 	
-	public function getLink()   
-	{
-		return  $this->link;
-	} 
-	
-	public function getUserInfo() 
+	public function getProfilePublications()
 	{
 		return  $this->data->entry_data->ProfilePage[0]->graphql->user;
-	} 
+	}
+	
 
-	public function getUserName() 
-	{
-		return  $this->data->entry_data->ProfilePage[0]->graphql->user->username;
-	}
 	
-	public function getUserFullName() 
-	{
-		return  $this->data->entry_data->ProfilePage[0]->graphql->user->full_name;
-	}
 
-    public function getUserPhoto()     
-	{
-		return  $this->data->entry_data->ProfilePage[0]->graphql->user->profile_pic_url_hd;
-	}
-	
-	public function getUserPhotoThumb()     
-	{
-		return  $this->data->entry_data->ProfilePage[0]->graphql->user->profile_pic_url;
-	}
-
-	public function getUserFollowers() 
-	{
-		return  $this->data->entry_data->ProfilePage[0]->graphql->user->edge_followed_by->count;
-	}
-
-	public function getUserFollows() 
-	{
-		return  $this->data->entry_data->ProfilePage[0]->graphql->user->edge_follow->count;
-	}
-	
-	public function getUserPublications() 
-	{
-		return  $this->data->entry_data->ProfilePage[0]->graphql->user;
-	}
-	
-	public function getUserDescription() 
-	{
-		return  $this->data->entry_data->ProfilePage[0]->graphql->user->biography;
-	}
-	
-	public function getUserSite() 
-	{
-		return  $this->data->entry_data->ProfilePage[0]->graphql->user->external_url;
-	}
 }
